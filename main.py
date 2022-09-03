1c751199f22dccf46ff689e249ec395d7736bff5
johnshaty <john.shashaty@student.fairfield.edu>
2022-09-03 11:25:29 -0400

    -Wordlength evaluation to fit user input -exception for faulty user input

--- main.py
+++ main.py
@@ -1,66 +1,71 @@
 import random
 
-#variables
+# variables
+
 Letters = 'abcdefghijklmnopqrstuvwxyz'
 UpperLetters = Letters.upper()
 Numbers = '0123456789'
 Symbols = '!@#$%^&*)?'
+combo2 = Numbers + Symbols
+
+# input password length
+
+while True:
+    try:
+        PassLength = int(input("Enter your password length: \n"))
+        break
+    except:
+        print("Error, please enter a valid number. ")
+
+# reads word file in
 
 with open('word_list') as word_file:
     english_words = (word_file.read().split())
 
-#Random word selection from file
+# Random word selection from file
 
 def wordIndex():
-#    with open('word_list') as word_file:
-#        english_words = (word_file.read().split())
-        word_index = random.randint(0, len(english_words))
+    word_index = random.randint(0, len(english_words))
 
-        return word_index
+    return word_index
 
 def wordSelect():
-#    with open('word_list') as word_file:
-#        english_words = (word_file.read().split())
-#        word_index = random.randint(0, len(english_words))
-    word_select = english_words[wordIndex()]
-    if len(word_select)-3 >= PassLength:
-        wordIndex()
-    else:
-        print(word_select)
-        print("""""""""""""""""")
-    return word_select
+
+    condition = False
+
+# Evaluating if word fits user length requirements
+
+    while condition == False:
+        word_select = english_words[wordIndex()]
+
+        if(len(word_select) != (PassLength - 3)):
+            wordIndex()
+
+            condition = False
+
+# iterate for every new word that does not fit condition
+
+        else:
+            print(word_select)
+            print("""""""""""""""""""")
+            condition = True
+            return word_select
 
 
+# Replacing certain letters with Symbols
 
-
-
-#Replacing certain letters with Symbols
 def processString(word):
-  dictionary = {'a': '@', 'A': '@', 'e': '3', 'E': '3'}
-  transTable = word.maketrans(dictionary)
-  Utxt = word.translate(transTable)
+    dictionary = {'a': '@', 'A': '@', 'e': '3', 'E': '3'}
+    transTable = word.maketrans(dictionary)
+    Utxt = word.translate(transTable)
 
-  return Utxt
-
-
+    return Utxt
 
 
-
-
-
-
-#input password length
-PassLength = int(input("Enter your password length: "))
-
-#combo = Letters + UpperLetters + Numbers + Symbols
-combo2 = Numbers + Symbols
-
-
-
-#GENERATED_PASSWORD = "".join(random.sample(combo, PassLength))
-
+# final password generation
 
 GENERATED_PASSWORD = processString(wordSelect()) + "".join(random.sample(combo2, 3))
 
-#print(processString(wordSelect()))
 print(GENERATED_PASSWORD)
+print(len(GENERATED_PASSWORD))
+
